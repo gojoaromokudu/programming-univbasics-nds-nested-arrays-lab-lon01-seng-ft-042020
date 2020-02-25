9a12f73804de9d301887b52156a1d9dcb5645a88
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-02-25 23:34:50 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,8 +33,8 @@ def sorted_matrix
   # However, this time, sort each internal array alphabetically by the first character
   
   array = [
-    CONVENTIONAL_PRODUCE.sort!,
-    ORGANIC_PRODUCE.sort!
+    CONVENTIONAL_PRODUCE.sort,
+    ORGANIC_PRODUCE.sort
     ]
   
 end
